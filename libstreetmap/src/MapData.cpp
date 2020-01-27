668c7f1f2c3897de6cbb7e08b8b0b78c083bd0b1
Hayden Goodfellow <Hayden.Goodfellow@mail.utoronto.ca>
2020-01-26 22:36:04 -0500

    Added streetIDsOfStreetName multimap and accessors/mutators to use it. Created function to find IDs from partial names. Almost works but it produces the correct answer 4x instead of one. Need to figure out why

--- libstreetmap/src/MapData.cpp
+++ libstreetmap/src/MapData.cpp
@@ -1,6 +1,8 @@
 #include "MapData.h"
 #include "m1.h"
 
+#include <algorithm>
+
 //==============================================================================
 // Constructors / Destructors
 //==============================================================================
@@ -27,21 +29,48 @@ void MapData::allocIntersectionVecs(const unsigned& numIntersections) {
 //==============================================================================
 // Mutators
 //==============================================================================
-void MapData::addIntersectToStreet(const IntersectionIndex& intId, const StreetIndex& streetId) {
-    intersectionsOfStreet[streetId].insert(intId);
+void MapData::addStreetIDtoName(const StreetIndex& streetID, const std::string& streetName) {
+    IDsOfStreetNames.insert(std::make_pair(streetName, streetID));
 }
-void MapData::addSegToIntersection(const StreetSegmentIndex& segId, const IntersectionIndex& intId) {
-    segsOfIntersection[intId].push_back(segId);
+void MapData::addIntersectToStreet(const IntersectionIndex& intID, const StreetIndex& streetID) {
+    intersectionsOfStreet[streetID].insert(intID);
+}
+void MapData::addSegToIntersection(const StreetSegmentIndex& segID, const IntersectionIndex& intID) {
+    segsOfIntersection[intID].push_back(segID);
 }
 //==============================================================================
 // Accessors
 //==============================================================================
-const std::vector<int> MapData::getIntersectionsOfStreet(const StreetIndex& streetId) const {
+const std::vector<int> MapData::getStreetIDsFromStreetName(std::string name) {
+    std::vector<int> streetIDs;
+    if (!name.empty()) {
+        auto itr = IDsOfStreetNames.lower_bound(name);
+        auto end = IDsOfStreetNames.end();
+        if (itr != end) {
+            std::string nameMatch = itr->first;
+            std::transform(nameMatch.begin(), nameMatch.end(), nameMatch.begin(), ::tolower);
+            std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+            
+            while (!nameMatch.compare(0, name.size(), name, 0, name.size())) {
+                streetIDs.push_back(itr->second);
+                itr++;
+                if (itr != end) {
+                    nameMatch = itr->first;
+                    std::transform(nameMatch.begin(), nameMatch.end(), nameMatch.begin(), ::tolower);
+                }
+                else if (itr == end)
+                    return streetIDs;
+            }
+        }
+    }
+    return streetIDs;
+}
+const std::vector<int> MapData::getIntersectionsOfStreet(const StreetIndex& streetID) const {
     std::vector<int> intersections;
-    for (int intId : intersectionsOfStreet[streetId])
-        intersections.push_back(intId);
+    for (int intID : intersectionsOfStreet[streetID])
+        intersections.push_back(intID);
     return intersections;
 }
-const std::vector<int> MapData::getSegsOfIntersection(const IntersectionIndex& intId) const {
-    return segsOfIntersection[intId];
+const std::vector<int> MapData::getSegsOfIntersection(const IntersectionIndex& intID) const {
+    return segsOfIntersection[intID];
 }
