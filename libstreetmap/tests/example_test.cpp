015b1b77bcdf71a92496a4cc5043b6b9ccd40861
Sean Kurian <sean.kurian@mail.utoronto.ca>
2020-01-29 18:21:01 -0500

    Added unit test function to test map comparitor and find IDs from partial name. Not sure whats causing the 4x bug

--- libstreetmap/tests/example_test.cpp
+++ libstreetmap/tests/example_test.cpp
@@ -19,8 +19,89 @@
  * SOFTWARE.
  */
 #include <unittest++/UnitTest++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include "mapComparitor.cpp"
 
 TEST(ExampleTest) {
     //This test will always pass
     CHECK(true);
 }
+
+TEST(MapComparitorTest) {
+    std::multimap<std::string, unsigned> defaultMap;
+    std::multimap<std::string, unsigned, mapCaseCompare> compareMap;
+    
+    std::string testName = "Saint";
+    unsigned num = 10;   
+    defaultMap.insert(std::make_pair(testName, num));
+    compareMap.insert(std::make_pair(testName, num));
+    
+    testName = "saint";
+    num = 20;   
+    defaultMap.insert(std::make_pair(testName, num));
+    compareMap.insert(std::make_pair(testName, num));
+    
+    testName = "sbint";
+    num = 30;   
+    defaultMap.insert(std::make_pair(testName, num));
+    compareMap.insert(std::make_pair(testName, num));
+    
+    testName = "Saint Toronto";
+    num = 40;   
+    defaultMap.insert(std::make_pair(testName, num));
+    compareMap.insert(std::make_pair(testName, num));
+    
+    testName = "SaintToronto";
+    num = 50;   
+    defaultMap.insert(std::make_pair(testName, num));
+    compareMap.insert(std::make_pair(testName, num));
+    
+    unsigned count = 0;
+    std::cout << "============== Default Map ==============\n";
+    for (auto itrd = defaultMap.begin(); itrd != defaultMap.end(); ++itrd) {
+        std::cout << "======= Element " << count << ": =======\nKey: " << itrd->first
+                  << "\nValue: " << itrd->second << "\n";
+        count++;
+    }
+    
+    count = 0;
+    std::cout << "============== Compare Map ==============\n";
+    for (auto itrc = compareMap.begin(); itrc != compareMap.end(); ++itrc) {
+        std::cout << "======= Element " << count << ": =======\nKey: " << itrc->first
+                  << "\nValue: " << itrc->second << "\n";
+        count++;
+    }
+    std::string name = "Sa";
+    std::vector<int> streetIDs;
+    if (!name.empty()) {
+        auto itr = compareMap.lower_bound(name);
+        auto end = compareMap.end();
+        if (itr != end) {
+            std::string nameMatch = itr->first;
+            std::transform(nameMatch.begin(), nameMatch.end(), nameMatch.begin(), ::tolower);
+            std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+            
+            while (!nameMatch.compare(0, name.size(), name, 0, name.size())) {
+                streetIDs.push_back(itr->second);
+                itr++;
+                if (itr != end) {
+                    nameMatch = itr->first;
+                    std::transform(nameMatch.begin(), nameMatch.end(), nameMatch.begin(), ::tolower);
+                }
+                else if (itr == end)
+                    break;
+            }
+        }
+    }
+    std::cout << "============== IDs Found ==============\n";
+    for (unsigned i = 0; i < streetIDs.size(); ++i) {
+        std::cout << streetIDs[i] << " ";
+    }
+    std::cout << "\n";
+    CHECK(true);
+}
+
