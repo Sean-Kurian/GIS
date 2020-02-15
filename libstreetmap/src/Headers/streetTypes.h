e82583a642695aa559e85654b5e4c8e4e38dce14
Hayden Goodfellow <Hayden.Goodfellow@mail.utoronto.ca>
2020-02-15 00:50:18 -0500

    Added segsOfWayOSMID to store segments of a given OSMID. Added mutators/accessors for that. Added enum class streetTypes to classify streets (e.g. highways, minorRoads etc..)

---
+++ libstreetmap/src/Headers/streetTypes.h
@@ -0,0 +1,14 @@
+#ifndef STREETTYPES_H
+#define STREETTYPES_H
+
+enum class streetType {
+    highway = 0,
+    majorRoad,
+    minorRoad,
+    path,
+    trail,
+    TYPECOUNT
+};
+
+#endif /* STREETTYPES_H */
+
