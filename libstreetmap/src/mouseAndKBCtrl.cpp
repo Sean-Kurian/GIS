0be79cab757b82d9d372042fd93f776031a4c04e
Sean Kurian <sean.kurian@mail.utoronto.ca>
2020-02-18 18:04:51 -0500

    Finds closest intersection on mouse click, need to finish highlighting intersection

--- libstreetmap/src/mouseAndKBCtrl.cpp
+++ libstreetmap/src/mouseAndKBCtrl.cpp
@@ -1,7 +1,12 @@
 #include "mouseAndKBCtrl.h"
+#include "m1.h"
+#include "drawMapHelpers.h"
+#include "LatLon.h"
 
 
 void actOnMousePress(ezgl::application* app, GdkEventButton* event, double x, double y) {
     app->update_message("Mouse Clicked");
     std::cout << "User clicked mouse at (" << x << "," << y << ")\n";
+    LatLon clicked = LatLon(latFromY(y), lonFromX(x));
+    std::cout << "Closest intersection: " << find_closest_intersection(clicked) << "\n";
 }
