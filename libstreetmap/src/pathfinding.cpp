6a029da28a39ae31a34e1670b36ec28920c1eedf
Sean Kurian <sean.kurian@mail.utoronto.ca>
2020-03-20 13:44:48 -0400

    implemented compute_path_walking_time

--- libstreetmap/src/pathfinding.cpp
+++ libstreetmap/src/pathfinding.cpp
@@ -1,6 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//==============================================================================
+// 
+//
+//==============================================================================
 
+
+#include "pathfinding.h"
+#include "m1.h"
+#include "StreetsDatabaseAPI.h"
+#include "OSMDatabaseAPI.h"
+#include "globalData.h"
+#include "math.h"
+
+double compute_path_travel_time(const std::vector<StreetSegmentIndex>& path, 
+        const double turn_penalty){
+    
+        if (path.size() == 0) return 0; 
+        
+        double travel_time = 0; 
+            InfoStreetSegment SSData, SSData2; 
+            for (int i = 0; i < path.size()-1; i++){
+                SSData = getInfoStreetSegment(path[i]); 
+                SSData2 = getInfoStreetSegment(path[i+1]); 
+
+                travel_time += find_street_segment_travel_time(path[i]); 
+                if(SSData.streetID != SSData2.streetID){
+                    travel_time += turn_penalty; 
+                } 
+            }
+            travel_time += find_street_segment_travel_time(path[path.size() - 1]); 
+            return travel_time; 
+}
+//length over walking speed
+double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, 
+        const double walking_speed, 
+        const double turn_penalty){
+    
+            if (path.size() == 0) return 0; 
+    
+            double travel_time = 0; 
+            InfoStreetSegment SSData, SSData2; 
+            for (int i = 0; i < path.size()-1; i++){
+                SSData = getInfoStreetSegment(path[i]); 
+                SSData2 = getInfoStreetSegment(path[i+1]); 
+
+                travel_time += find_street_segment_length(path[i]) / walking_speed; 
+                if(SSData.streetID != SSData2.streetID){
+                    travel_time += turn_penalty; 
+                } 
+            }
+            travel_time += find_street_segment_length(path[path.size() - 1]) / 1; 
+            return travel_time; 
+}
+
+double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, 
+        const double walking_speed, const double turn_penalty); 
+
+std::vector<StreetSegmentIndex> find_path_between_intersections(
+        const IntersectionIndex intersect_id_start,
+            const IntersectionIndex intersect_id_end, 
+            const double turn_penalty){
+    
+}
+
+std::pair<std::vector<StreetSegmentIndex>, std::vector<StreetSegmentIndex>> find_path_with_walk_to_pickup(
+        const IntersectionIndex start_intersection, 
+        const IntersectionIndex end_intersection, 
+        const double turn_penalty, 
+        const double walking_speed, 
+        const double walking_time_limit){
+    
+}
