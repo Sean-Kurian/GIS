5277b1d3f476b53fbe8944baaf2c563604063c57
Sean Kurian <sean.kurian@mail.utoronto.ca>
2020-03-20 12:39:33 -0400

    formatting fixes

--- libstreetmap/src/Headers/pathfinding.h
+++ libstreetmap/src/Headers/pathfinding.h
@@ -15,7 +15,8 @@ double compute_path_travel_time(const std::vector<StreetSegmentIndex>& path, con
 std::vector<StreetSegmentIndex> find_path_between_intersections(const IntersectionIndex intersection_id_start, 
             const IntersectionIndex intersect_id_end, const double turn_penalty);   
 
-double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, const double walking_speed, const double turn_penalty); 
+double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, 
+        const double walking_speed, const double turn_penalty); 
 
 std::pair<std::vector<StreetSegmentIndex>, std::vector<StreetSegmentIndex>> find_path_with_walk_to_pickup(
         const IntersectionIndex start_intersection, 
