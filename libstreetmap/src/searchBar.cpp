a3315883017893d3bedaa84b745c3cb8e7612035
Sean <sean.kuria@mail.utoronto.ca>
2020-03-26 19:31:51 -0400

    Commenting and style

--- libstreetmap/src/searchBar.cpp
+++ libstreetmap/src/searchBar.cpp
@@ -1,8 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//==============================================================================
+// File Description: All functions related to search with searchbars
+//
+//==============================================================================
 
 #include "ezgl/graphics.hpp"
 #include "ezgl/application.hpp"
