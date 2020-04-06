44d3da99b77fe70fe47e89544d58e8d18eab2719
Sean Kurian <sean.kurian@mail.utoronto.ca>
2020-04-06 17:49:35 -0400

    Implemented simple, crappy greedy algorithm for the m4 pathfinding function

--- libstreetmap/src/m4.cpp
+++ libstreetmap/src/m4.cpp
@@ -1,9 +1,54 @@
+#include "m1.h"
 #include "m4.h"
 #include "pathfinding.h"
+
+#include <limits>
+#include <cstdlib>
+#include <unordered_set>
 
 std::vector<CourierSubpath> traveling_courier(const std::vector<DeliveryInfo>& deliveries,
                                               const std::vector<int>& depots,
                                               const float turn_penalty,
                                               const float truck_capacity) {
+    unsigned randDepot = std::rand() % depots.size();
+    unsigned currentInt = depots[randDepot];
+    std::vector<bool> orderComplete;
+    orderComplete.resize(deliveries.size());
+    unsigned numToComplete = deliveries.size();
+    unsigned numCompleted = 0;
     
+    std::vector<CourierSubpath> result;
+
+    while (numCompleted < numToComplete) {
+        CourierSubpath toPickup, toDropoff;
+        double closestLocation = std::numeric_limits<double>::max();
+        unsigned closestOrder = 0;
+        for (unsigned orderNum = 0; orderNum < numToComplete; ++orderNum) {
+            if (!orderComplete[orderNum]) {
+                double distToOrder = find_distance_between_two_points(std::make_pair(
+                                     getIntersectionPosition(currentInt),
+                                     getIntersectionPosition(deliveries[orderNum].pickUp)));
+                if (closestLocation < distToOrder) 
+                    closestOrder = orderNum;
+            }
+        }
+        toPickup.start_intersection = currentInt;
+        toPickup.end_intersection = deliveries[closestOrder].pickUp;
+        toPickup.subpath = find_path_between_intersections(currentInt, 
+                                                           deliveries[closestOrder].pickUp, 
+                                                           turn_penalty);
+        result.push_back(toPickup);
+        
+        toDropoff.start_intersection = deliveries[closestOrder].pickUp;
+        toDropoff.end_intersection = deliveries[closestOrder].dropOff;
+        toDropoff.subpath = find_path_between_intersections(deliveries[closestOrder].pickUp,
+                                                            deliveries[closestOrder].dropOff,
+                                                            turn_penalty);
+        toDropoff.pickUp_indices.push_back(deliveries[closestOrder].pickUp);
+        result.push_back(toDropoff);
+        
+        orderComplete[closestOrder] = true;
+        currentInt = deliveries[closestOrder].dropOff;
+        numCompleted++;
+    }
 }
