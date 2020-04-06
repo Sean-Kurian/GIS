0c6f2d7a678136e4335d28520b7ddb66a949b354
Hayden Goodfellow <Hayden.Goodfellow@mail.utoronto.ca>
2020-04-06 17:49:35 -0400

    Implemented simple, crappy greedy algorithm for the m4 pathfinding function

--- libstreetmap/src/m4Helpers.cpp
+++ libstreetmap/src/m4Helpers.cpp
@@ -1,24 +1,2 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   m4Helpers.cpp
- * Author: kurianse
- * 
- * Created on April 6, 2020, 4:47 PM
- */
-
 #include "m4Helpers.h"
-
-m4Helpers::m4Helpers() {
-}
-
-m4Helpers::m4Helpers(const m4Helpers& orig) {
-}
-
-m4Helpers::~m4Helpers() {
-}
 
