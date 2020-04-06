8a7f3036324e09f302a40ba32d581f313c674ca8
Sean Kurian <sean.kurian@mail.utoronto.ca>
2020-04-06 16:47:56 -0400

    Created m4Helpers header and source

---
+++ libstreetmap/src/Headers/m4Helpers.h
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+#ifndef M4HELPERS_H
+#define M4HELPERS_H
+
+#endif /* M4HELPERS_H */
+
